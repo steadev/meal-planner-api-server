8bffad39ddaa9063021a4e812d8ae04057e557b9
MyungJoo Lee <audwn0326@gmail.com>
2025-02-05 15:09:06 +0900

    first commit

---
+++ src/main/kotlin/steadev/meal_planner/MealPlannerApplication.kt
@@ -0,0 +1,11 @@
+package steadev.meal_planner
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MealPlannerApplication
+
+fun main(args: Array<String>) {
+	runApplication<MealPlannerApplication>(*args)
+}
---
+++ src/test/kotlin/steadev/meal_planner/MealPlannerApplicationTests.kt
@@ -0,0 +1,13 @@
+package steadev.meal_planner
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MealPlannerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
